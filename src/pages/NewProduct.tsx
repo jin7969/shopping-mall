ca89e9051a3dd03a9478cedf655aabafe77429a3
Ahn <tn3748@naver.com>
2023-04-25 22:09:42 +0900

    feat: 새로운 상품 등록 페이지 스타일 변경

--- src/pages/NewProduct.tsx
+++ src/pages/NewProduct.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { AiOutlineFileImage } from "react-icons/ai";
 
 interface ProductData {
   title: string;
@@ -25,84 +26,87 @@ function NewProduct() {
 
     if (name === "file") {
       if (files?.length === 0) return;
-
       setFile(files && files[0]);
-      console.log(files);
       return;
     }
-
     setProduct((product) => ({ ...product, [name]: value }));
   };
 
   const handleSubmit = (e: React.FormEvent) => {};
 
   return (
-    <section className="flex justify-center items-center">
-      <div>
-        <h2 className="text-center text-2xl font-bold my-4">
-          새로운 제품 등록
-        </h2>
-        {success && <p className="my-2">✅ {success}</p>}
+    <section>
+      <h1 className="text-center text-2xl font-bold my-6">새로운 상품 등록</h1>
+      {success && <p className="my-2">✅ {success}</p>}
+      <div className="flex justify-center items-center ">
+        {!file && (
+          <div className="flex justify-center items-center w-96 h-[30rem] border-2 text-5xl rounded-xl">
+            <AiOutlineFileImage color="#9f9f9f" />
+          </div>
+        )}
         {file && (
           <img
-            className="w-96 rounded-xl"
+            className="w-96 h-[30rem] rounded-xl"
             src={URL.createObjectURL(file)}
-            alt="local file"
+            alt="상품 이미지"
           />
         )}
+        <form className="flex flex-col px-14" onSubmit={handleSubmit}>
+          <input
+            type="file"
+            accept="image/*"
+            name="file"
+            required
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="title"
+            value={product.title ?? ""}
+            placeholder="상품명"
+            required
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            name="price"
+            value={product.price ?? ""}
+            placeholder="가격"
+            required
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="category"
+            value={product.category ?? ""}
+            placeholder="카테고리"
+            required
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="description"
+            value={product.description ?? ""}
+            placeholder="상품 설명"
+            required
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="options"
+            value={product.options ?? ""}
+            placeholder="콤마(,)로 구분 / ex) S,M,L"
+            required
+            onChange={handleChange}
+          />
+          <button
+            className="py-5 bg-rose-400 text-white font-bold"
+            disabled={isUploading}
+          >
+            {isUploading ? "업로드중..." : "상품 등록하기"}
+          </button>
+        </form>
       </div>
-      <form className="flex flex-col px-12" onSubmit={handleSubmit}>
-        <input
-          type="file"
-          accept="image/*"
-          name="file"
-          required
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="title"
-          value={product.title ?? ""}
-          placeholder="제품명"
-          required
-          onChange={handleChange}
-        />
-        <input
-          type="number"
-          name="price"
-          value={product.price ?? ""}
-          placeholder="가격"
-          required
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="category"
-          value={product.category ?? ""}
-          placeholder="카테고리"
-          required
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="description"
-          value={product.description ?? ""}
-          placeholder="제품 설명"
-          required
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="options"
-          value={product.options ?? ""}
-          placeholder="콤마(,)로 구분 / ex) S,M,L"
-          required
-          onChange={handleChange}
-        />
-        <button disabled={isUploading}>
-          {isUploading ? "업로드중..." : "제품 등록하기"}
-        </button>
-      </form>
     </section>
   );
 }
