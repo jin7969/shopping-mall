6f226df3306adc0524d3236bf8551d29415167bb
Ahn <tn3748@naver.com>
2023-04-27 06:19:56 +0900

    feat: footer 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthContextProvider } from "./context/AuthContext";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   const queryClient = new QueryClient();
@@ -11,6 +12,7 @@ function App() {
       <AuthContextProvider>
         <Navbar />
         <Outlet />
+        <Footer />
       </AuthContextProvider>
     </QueryClientProvider>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import {
+  AiOutlineHome,
+  AiOutlineMenu,
+  AiOutlineSearch,
+  AiOutlineUser,
+} from "react-icons/ai";
+import { ROUTES } from "../constants";
+
+const footerMenu = [
+  { id: "1", link: ROUTES.HOME, name: "홈", icon: <AiOutlineHome /> },
+  { id: "2", link: ROUTES.PRODUCTS, name: "전체보기", icon: <AiOutlineMenu /> },
+  { id: "3", link: ROUTES.HOME, name: "검색", icon: <AiOutlineSearch /> },
+  {
+    id: "4",
+    link: ROUTES.MY_PAGE,
+    name: "마이페이지",
+    icon: <AiOutlineUser />,
+  },
+];
+
+function Footer() {
+  return (
+    <footer className="w-[42rem] fixed left-50% bottom-0">
+      <ul className="flex justify-around py-1 bg-gray-50 text-gray-500 text-2xl">
+        {footerMenu.map((menu) => (
+          <Link to={menu.link} className="flex flex-col items-center">
+            {menu.icon}
+            <span className="text-sm">{menu.name}</span>
+          </Link>
+        ))}
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
