d579aebe0c46b91f1aa1105a53aff74d90794659
Ahn <tn3748@naver.com>
2023-04-27 07:25:41 +0900

    feat: 상품 카드 스타일링

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,7 +5,14 @@ function ProductCard({
 }: {
   product: ProductData;
 }) {
-  return <div>{title}</div>;
+  return (
+    <li className="rounded-lg overflow-hidden">
+      <img src={image} alt="상품 이미지" />
+      <h3 className="font-semibold">{`${price.toLocaleString()}원`}</h3>
+      <p className="text-zinc-700 text-xs truncate">{title}</p>
+      <span className="text-zinc-500 text-xs">{category}</span>
+    </li>
+  );
 }
 
 export default ProductCard;
