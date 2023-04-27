98f4aaad52c4486c124dedbadb879a1c5f71f420
Ahn <tn3748@naver.com>
2023-04-27 16:50:53 +0900

    feat: 장바구니 페이지 구현

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,37 @@
+import { AiOutlineDelete, AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { CartProductData } from "../types";
+
+interface CartItemProps {
+  product: CartProductData;
+  uid: string;
+}
+
+function CartItem({
+  product: { id, image, title, option, quantity, price },
+  uid,
+}: CartItemProps) {
+  return (
+    <li className="flex p-2 bg-gray-50">
+      <input type="checkbox" />
+      <img src={image} alt="상품 이미지" className="w-28 h-28 ml-4" />
+      <div className="flex-1 p-3">
+        <p>{title}</p>
+        <p>{option}</p>
+      </div>
+      <div className="p-2">
+        <AiOutlineDelete
+          size={28}
+          className="ml-auto mb-2 p-1 bg-gray-200 rounded-full"
+        />
+        <div className="flex items-center">
+          <AiOutlineMinus size={20} />
+          <span className="px-3 text-lg">{quantity}</span>
+          <AiOutlinePlus size={20} />
+        </div>
+        <p className="pt-2 text-lg font-semibold">{`${price.toLocaleString()}원`}</p>
+      </div>
+    </li>
+  );
+}
+
+export default CartItem;
--- src/pages/MyCart.tsx
+++ src/pages/MyCart.tsx
@@ -1,8 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import { useAuthContext } from "../context/AuthContext";
+import { getCart } from "../api/firebase";
+import CartItem from "../components/CartItem";
+
 function MyCart() {
+  const { uid } = useAuthContext();
+  const { isLoading, data: products } = useQuery(["carts"], () => getCart(uid));
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (products && products.length === 0) {
+    return (
+      <section>
+        <p>장바구니가 비어있어요</p>
+        <button>상품 보러가기</button>
+      </section>
+    );
+  }
+
   return (
-    <div>
-      <div>cart</div>
-    </div>
+    <section className="flex flex-col items-center">
+      <h1 className="py-3 text-xl font-bold">장바구니</h1>
+      <ul className="flex flex-col w-full gap-3">
+        {products?.map((product) => (
+          <CartItem key={product.id} product={product} uid={uid} />
+        ))}
+      </ul>
+    </section>
   );
 }
 
