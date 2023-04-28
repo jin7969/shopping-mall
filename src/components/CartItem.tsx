46b5e15bbfaf18d1321501042de8434cefb6d7eb
Ahn <tn3748@naver.com>
2023-04-28 23:57:00 +0900

    feat: cart item 수량 변경 및 삭제 기능 추가

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,5 +1,7 @@
 import { AiOutlineDelete, AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { CartProductData } from "../types";
+import { addOrUpdateToCart } from "../api/firebase";
+import { removeFromCart } from "../api/firebase";
 
 interface CartItemProps {
   product: CartProductData;
@@ -7,12 +9,23 @@ interface CartItemProps {
 }
 
 function CartItem({
+  product,
   product: { id, image, title, option, quantity, price },
   uid,
 }: CartItemProps) {
+  const handleMinusQuantity = () => {
+    if (quantity === 1) return;
+    addOrUpdateToCart(uid, { ...product, quantity: quantity - 1 });
+  };
+
+  const handlePlusQuantity = () =>
+    addOrUpdateToCart(uid, { ...product, quantity: quantity + 1 });
+
+  const handleDeleteCartItem = () => removeFromCart(uid, id);
+
   return (
     <li className="flex p-2 bg-gray-50">
-      <input type="checkbox" />
+      <input type="checkbox" className="w-5 accent-brand" />
       <img src={image} alt="상품 이미지" className="w-28 h-28 ml-4" />
       <div className="flex-1 p-3">
         <p>{title}</p>
@@ -22,11 +35,12 @@ function CartItem({
         <AiOutlineDelete
           size={28}
           className="ml-auto mb-2 p-1 bg-gray-200 rounded-full"
+          onClick={handleDeleteCartItem}
         />
         <div className="flex items-center">
-          <AiOutlineMinus size={20} />
+          <AiOutlineMinus size={20} onClick={handleMinusQuantity} />
           <span className="px-3 text-lg">{quantity}</span>
-          <AiOutlinePlus size={20} />
+          <AiOutlinePlus size={20} onClick={handlePlusQuantity} />
         </div>
         <p className="pt-2 text-lg font-semibold">{`${price.toLocaleString()}원`}</p>
       </div>
