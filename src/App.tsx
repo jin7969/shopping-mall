0c3ab099c66550be31bac3c1377f7e5978a6ea63
Ahn <tn3748@naver.com>
2023-05-06 03:28:08 +0900

    feat: snackbar context 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthContextProvider } from "./context/AuthContext";
+import { SnackbarProvider } from "./context/SnackbarContext";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
@@ -11,10 +12,12 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
-        <ScrollToTop />
-        <Navbar />
-        <Outlet />
-        <Footer />
+        <SnackbarProvider>
+          <ScrollToTop />
+          <Navbar />
+          <Outlet />
+          <Footer />
+        </SnackbarProvider>
       </AuthContextProvider>
     </QueryClientProvider>
   );
---
+++ src/context/SnackbarContext.tsx
@@ -0,0 +1,43 @@
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import Snackbar from "../components/Snackbar";
+
+interface SnackbarContextType {
+  showSnackbar: (message: string) => void;
+}
+
+const SnackbarContext = createContext<SnackbarContextType>({
+  showSnackbar: () => {},
+});
+
+const SnackbarProvider = ({ children }: { children: ReactNode }) => {
+  const [snackbarInfo, setSnackbarInfo] = useState("11");
+
+  const showSnackbar = (message: string) => {
+    setSnackbarInfo(message);
+  };
+
+  useEffect(() => {
+    const snackbarTimer = setTimeout(() => {
+      setSnackbarInfo("");
+    }, 2000);
+
+    return () => clearTimeout(snackbarTimer);
+  }, [snackbarInfo]);
+
+  return (
+    <SnackbarContext.Provider value={{ showSnackbar }}>
+      {children}
+      {snackbarInfo && <Snackbar key={Date.now()} message={snackbarInfo} />}
+    </SnackbarContext.Provider>
+  );
+};
+
+const useSnackbarContext = () => useContext(SnackbarContext);
+
+export { SnackbarProvider, useSnackbarContext };
