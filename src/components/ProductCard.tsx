b38770125be4721bd99fbe3aac4c1bf66ddfb88b
Ahn <tn3748@naver.com>
2023-05-05 20:58:12 +0900

    refactor: 상품 이미지 넓이 지정

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -14,7 +14,7 @@ function ProductCard({ product }: { product: ProductData }) {
         navigate(`${ROUTES.PRODUCTS}/${id}`, { state: { product } })
       }
     >
-      <img src={image} alt="상품 이미지" className="h-64 rounded-md" />
+      <img src={image} alt="상품 이미지" className="w-52 h-64 rounded-md" />
       <h3 className="font-semibold">{`${price.toLocaleString()}원`}</h3>
       <p className="text-zinc-700 text-xs truncate">{title}</p>
       <span className="text-zinc-500 text-xs">{category}</span>
