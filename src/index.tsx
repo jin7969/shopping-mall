2094ebcf011701719fd94c6f852f4754e5925b63
Ahn <tn3748@naver.com>
2023-05-07 01:19:45 +0900

    chore: gh-pages 설치 및 basename 설정

--- src/index.tsx
+++ src/index.tsx
@@ -12,38 +12,41 @@ import NewProduct from "./pages/NewProduct";
 import ProtectedRoute from "./pages/ProtectedRoute";
 import MyPage from "./pages/MyPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <NotFound description="잘못된 경로입니다." />,
-    children: [
-      { index: true, path: ROUTES.HOME, element: <Home /> },
-      { path: ROUTES.PRODUCTS, element: <AllProducts /> },
-      { path: `${ROUTES.PRODUCTS}/:id`, element: <ProductDetail /> },
-      {
-        path: ROUTES.NEW_PRODUCT,
-        element: (
-          <ProtectedRoute
-            requireAdmin
-            description="해당 페이지에 접근할 수 없습니다."
-          >
-            <NewProduct />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: ROUTES.MY_CART,
-        element: (
-          <ProtectedRoute description="로그인이 필요합니다.">
-            <MyCart />
-          </ProtectedRoute>
-        ),
-      },
-      { path: ROUTES.MY_PAGE, element: <MyPage /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <NotFound description="잘못된 경로입니다." />,
+      children: [
+        { index: true, path: ROUTES.HOME, element: <Home /> },
+        { path: ROUTES.PRODUCTS, element: <AllProducts /> },
+        { path: `${ROUTES.PRODUCTS}/:id`, element: <ProductDetail /> },
+        {
+          path: ROUTES.NEW_PRODUCT,
+          element: (
+            <ProtectedRoute
+              requireAdmin
+              description="해당 페이지에 접근할 수 없습니다."
+            >
+              <NewProduct />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: ROUTES.MY_CART,
+          element: (
+            <ProtectedRoute description="로그인이 필요합니다.">
+              <MyCart />
+            </ProtectedRoute>
+          ),
+        },
+        { path: ROUTES.MY_PAGE, element: <MyPage /> },
+      ],
+    },
+  ],
+  { basename: process.env.PUBLIC_URL }
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
